ee7d7b063fd02f0e3c0b7703badd969f1232ccef
MuhammadRadikal <iikal8959@gmail.com>
2024-01-03 23:33:50 +0700

    seventh commit

--- appcore/settings.py
+++ appcore/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&fn^xt##uv)!s&yoj+82dgk$e5bvn)l_!6v%&f6utz+i+kt020'
+SECURE_SSL_REDIRECT = True
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

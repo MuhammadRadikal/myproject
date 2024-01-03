53ac484566cad2f9ceff555737b5497140c674ab
MuhammadRadikal <iikal8959@gmail.com>
2024-01-03 23:12:01 +0700

    eigth commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Artikel
 
 def Home(request):
-    return render(request, 'blog/home.html')
+    List_Artikel = Artikel.objects.all()
+    context = {
+        'list_artikel' : List_Artikel
+    }
+    return render(request, 'blog/home.html', context)
